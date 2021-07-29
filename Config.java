d2f3714291f60f44473a313a7f0a6b8a163141be
Scott Krahl <83058231+KrahlS@users.noreply.github.com>
2021-07-29 18:06:24 -0500

    Added class description and reformat

--- Config.java
+++ Config.java
@@ -1,24 +1,18 @@
-
+/**
+ * This class contains three preset board templates for use in the GameOfLife class file.
+ * 
+ * @author Scott
+ *
+ */
 public class Config {
-
-
-  public static final int[][] TOAD = {{0, 0, 0, 0, 0, 0}, 
-                                      {0, 0, 0, 0, 0, 0}, 
-                                      {0, 0, 1, 1, 1, 0},
-                                      {0, 1, 1, 1, 0, 0}, 
-                                      {0, 0, 0, 0, 0, 0}, 
-                                      {0, 0, 0, 0, 0, 0}};
   
-  public static final int[][] BEEHIVE = {{0, 0, 0, 0, 0, 0}, 
-                                         {0, 0, 1, 1, 0, 0}, 
-                                         {0, 1, 0, 0, 1, 0},
-                                         {0, 0, 1, 1, 0, 0}, 
-                                         {0, 0, 0, 0, 0, 0}};
+  public static final int[][] TOAD = {{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 1, 1, 1, 0},
+      {0, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
   
-  public static final int[][] GLIDER =  {{0, 1, 0, 0, 0, 0}, 
-                                         {0, 0, 1, 0, 0, 0}, 
-                                         {1, 1, 1, 0, 0, 0},
-                                         {0, 0, 0, 0, 0, 0}, 
-                                         {0, 0, 0, 0, 0, 0}};
-                                      
+  public static final int[][] BEEHIVE = {{0, 0, 0, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 1, 0, 0, 1, 0},
+      {0, 0, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0}};
+  
+  public static final int[][] GLIDER = {{0, 1, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
 }
+
